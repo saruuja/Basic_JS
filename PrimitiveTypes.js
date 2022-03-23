0a3ef941859ed2a1b40f9084de394b6c38b8a77f
saruuja <saaruuja42@gmail.com>
2022-03-23 14:10:39 +0300

    Code

---
+++ PrimitiveTypes.js
@@ -0,0 +1,21 @@
+// Primitive types string,num,bolean
+
+let Fnme = 'Sara'; Lnme = 'Mohamad'; Age = 22;
+console.log(" My Name is " + Fnme + " " +  Lnme + " My age is " + Age + " years old "); 
+
+
+let x = 8;
+let y = 14;
+let c = x+y;
+console.log("Sum of x and y is ",c);
+ let name1 = 'Anas';
+let name2 = "Binu Maalik";
+let names = name1 + " " + name2;
+console.log(names);
+ const getName = 'what\'s your name';
+console.log(getName);
+//bolean
+const  Somalia = true;
+console.log(typeof Somalia)
+
+
